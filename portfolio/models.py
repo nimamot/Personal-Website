467b3778e2e154b813017068672c6e2386660c86
nimamot <64922998+nimamot@users.noreply.github.com>
2020-12-21 12:38:24 -0800

    New project added through admin

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=50)
+    description = models.CharField(max_length=1000)
     image = models.ImageField(upload_to='portfolio/images/')
     url = models.URLField(blank=True)
 
