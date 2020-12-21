d20b4e858e3072ca4ce45512ab6c8dcf79ca6cb9
nimamot <64922998+nimamot@users.noreply.github.com>
2020-12-21 00:10:12 -0800

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from . import views
+from django.urls import path
+
+
+app_name = 'blog'
+urlpatterns = [
+    path('', views.all_blogs, name='all_blogs'),
+    path('<int:blog_id>/', views.detail, name='detail')
+]
